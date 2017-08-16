01c74f31c2fb0d13baec36cb38a0e7b1ae84d530
Alex Kildeychik <alex.kildeichik@gmail.com>
2017-08-16 17:19:17 +0200

    Auth exception commit.

--- app/Http/Controllers/CatalogsController.php
+++ app/Http/Controllers/CatalogsController.php
@@ -9,7 +9,7 @@ class CatalogsController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth')->only('store');
+        $this->middleware('auth')->except(['index', 'show']);
     }
 
     /**
